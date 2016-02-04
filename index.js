2f482400632b4e59b73355f32858627631ab84f0
Robert Pearce <me@robertwpearce.com>
2016-02-04 10:07:17 +0000

    breaking out to allow for different layouts

--- index.js
+++ index.js
@@ -2,59 +2,68 @@ import fsp from 'fs-promise';
 import hogan from 'hogan.js';
 import walk from 'walkdir';
 import path from 'path';
-import { layoutFilename, outputDir, sourceDir, viewsDir } from './config';
+import { outputDir, sourceDir } from './config';
+
+const viewsDir = path.join(sourceDir, 'views');
 
 (async () => {
   try {
-    const viewsPath = path.join(sourceDir, viewsDir);
-
-    /*
-     * Create the initial output directory
-     */
     await fsp.mkdir(outputDir);
-
-    /*
-     * Read in and compile the layout file
-     */
-    const layoutPath = path.join(viewsPath, layoutFilename);
-    const layout = await fsp.readFile(layoutPath);
-    const layoutTemplate = hogan.compile(layout.toString());
-
-    /*
-     * Set up an even emitter for walking the source directory
-     */
-    const walkEmitter = walk(path.join(viewsPath));
-
-    /*
-     * When you come across a directory,
-     * create the same one in the outputDir.
-     */
-    walkEmitter.on('directory', async (dirpath, stat) => {
-      try {
-        await fsp.mkdir(path.join(outputDir, path.relative(viewsPath, dirpath)));
-      } catch (err) {
-        console.error(err);
-      }
-    });
-
-    /*
-     * When you come across a file that isn't the layout file,
-     * read it, compile it, and write the output to the outputDir.
-     */
-    walkEmitter.on('file', async (filepath, stat) => {
-      const relativePath = path.relative(viewsPath, filepath);
-      if (!(relativePath.match(layoutFilename))) {
-        try {
-          const view = await fsp.readFile(filepath);
-          const viewTemplate = hogan.compile(view.toString());
-          const rendered = viewTemplate.render({}, { layout: layoutTemplate });
-          await fsp.writeFile(path.join(outputDir, relativePath), rendered, 'utf-8');
-        } catch (err) {
-          console.error(err);
-        }
-      }
-    });
+    writeViews();
+    writePosts();
   } catch (err) {
     console.error(err);
   }
 })();
+
+const compileLayout = async (filename) => {
+  try {
+    const layoutPath = path.join(sourceDir, 'layouts', filename);
+    const layoutFile = await fsp.readFile(layoutPath);
+    return hogan.compile(layoutFile.toString());
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const writeViews = async () => {
+  const layout = await compileLayout('main.html');
+  const walkEmitter = walk(viewsDir);
+
+  /*
+   * When you come across a directory,
+   * create the same one in the outputDir.
+   */
+  walkEmitter.on('directory', async (dirpath, stat) => {
+    try {
+      await fsp.mkdir(path.join(outputDir, path.relative(viewsDir, dirpath)));
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  /*
+   * When you come across an HTML file,
+   * compile it and write it to the output folder.
+   */
+  walkEmitter.on('file', async (filepath, stat) => {
+    const relativePath = path.relative(viewsDir, filepath);
+    if (/\.html/.test(relativePath)) {
+      try {
+        const view = await fsp.readFile(filepath);
+        const viewTemplate = hogan.compile(view.toString());
+        const rendered = viewTemplate.render({}, { layout });
+        console.log(layout);
+        await fsp.writeFile(path.join(outputDir, relativePath), rendered, 'utf-8');
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  });
+}
+
+const writePosts = async () => {
+  //const layout = await compileLayout('blog.html');
+
+  //const walkEmitter = walk(path.join(viewsDir));
+}
