15490c35b3ec33850592f1ac3413195e2150527c
Robert Pearce <me@robertwpearce.com>
2016-02-15 14:00:55 +0000

    Refactored and simplified design. Removed blog index page (not necessary?)

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import { outputDir } from './config';
 import { readPosts, writePosts } from './lib/posts';
 import { writeViews } from './lib/views';
 import { writeCSS } from './lib/css';
+import { copyImages } from './lib/images';
 
 (async () => {
   try {
     await fsp.mkdir(outputDir);
     writeCSS();
+    copyImages();
     const posts = await readPosts();
     writePosts(posts);
     writeViews(posts);
