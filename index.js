405c62254b0d5c46f49a2d9808863582b4af57d5
Robert Pearce <me@robertwpearce.com>
2016-02-16 11:41:41 +0000

    build CNAME into _site

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import fs from 'fs';
 import fsp from 'fs-promise';
+import path from 'path';
 import { outputDir } from './config';
 import { readPosts, writePosts } from './lib/posts';
 import { writeViews } from './lib/views';
@@ -8,6 +10,7 @@ import { copyImages } from './lib/images';
 (async () => {
   try {
     await fsp.mkdir(outputDir);
+    fs.createReadStream('CNAME').pipe(fs.createWriteStream(path.join(outputDir, 'CNAME')));
     writeCSS();
     copyImages();
     const posts = await readPosts();
