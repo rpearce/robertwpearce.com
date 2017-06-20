caf214afd66f9bc68166e5ca42d7126ffa6b9a2e
Robert Pearce <me@robertwpearce.com>
2017-06-21 10:08:14 +1200

    Added signup form

--- src/js/components/home/index.js
+++ src/js/components/home/index.js
@@ -29,7 +29,26 @@ module.exports = ({ posts, reviews }) => `
         <header>
           <h2 class="heading--bordered">Blog Posts</h2>
         </header>
-        ${postList(posts)}
+        <section>
+          <!-- Begin MailChimp Signup Form -->
+          <form action="//robertwpearce.us13.list-manage.com/subscribe/post?u=2df44e8960266388bff165fa6&amp;id=ee2f2a3737" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate class="form">
+            <div id="mce-responses">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2df44e8960266388bff165fa6_ee2f2a3737" tabindex="-1" value=""></div>
+            <div class="form__container">
+              <label for="mce-EMAIL" class="form__label">Email Address</label>
+              <input type="email" value="" id="mce-EMAIL" class="form__input" name="EMAIL" placeholder="you@greatemail.com">
+              <input type="submit" value="Subscribe" name="subscribe" class="form__submit">
+            </div>
+            <small>Note: I will <em>never</em> give out your email nor spam you – that ain't right.</small>
+          </form>
+          <!--End mc_embed_signup-->
+        </section>
+        <section class="spaceUp">
+          ${postList(posts)}
+        </section>
       </section>
     </div>
   </main>
