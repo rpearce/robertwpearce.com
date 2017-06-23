c81adc86e15aa8e5b6fa73d08796375069d1a1a2
Robert Pearce <me@robertwpearce.com>
2017-06-24 10:53:41 +1200

    take out google analytics

--- src/config.js
+++ src/config.js
@@ -12,15 +12,20 @@ module.exports = {
     <meta name="google-site-verification" content="QX4bn65yOXBfgnwxlO09yIBc1H8blur-Erx3lmsDVhU">
     <link href="//fonts.googleapis.com/css?family=Lato:400" rel="stylesheet" type="text/css">
   `,
-  footerExtra: `
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-54506099-1', 'auto');
-      ga('send', 'pageview');
-    </script>
-  `
+  footerExtra: ``
 }
+
+/**
+ * If you want google analytics, then add this to the footerExtra.
+ * However, I'm just not comfortable adding this overhead to users
+ * and also tracking every goddamn thing they're doing.
+ */
+//<script>
+//  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+//  ga('create', 'UA-54506099-1', 'auto');
+//  ga('send', 'pageview');
+//</script>
