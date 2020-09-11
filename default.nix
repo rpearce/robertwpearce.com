f5a8dbbe51cccc84bc84360fd59efda07cc41cd4
Robert Pearce <me@robertwpearce.com>
2020-09-11 08:46:47 -0400

    ignore dist folder & try not using gitignoreSource

--- default.nix
+++ default.nix
@@ -1,28 +1,29 @@
 let
   sources = import ./nix/sources.nix;
 in
-  { pkgs ? import sources.nixpkgs { } }:
+{ pkgs ? import sources.nixpkgs {} }:
 
   let
-    generator = (import ./nix/default.nix { }).generator;
-  in pkgs.stdenv.mkDerivation {
-    name = "robertwpearce-com-site";
-    buildInputs = [
-      generator
-    ];
-    src = ./src;
+    generator = (import ./nix/default.nix {}).generator;
+  in
+    pkgs.stdenv.mkDerivation {
+      name = "robertwpearce-com-site";
+      buildInputs = [
+        generator
+      ];
+      src = ./src;
 
-    # https://github.com/jaspervdj/hakyll/issues/614
-    # https://github.com/NixOS/nix/issues/318#issuecomment-52986702
-    # https://github.com/MaxDaten/brutal-recipes/blob/source/default.nix#L24
-    LOCALE_ARCHIVE = pkgs.lib.optionalString (pkgs.buildPlatform.libc == "glibc") "${pkgs.glibcLocales}/lib/locale/locale-archive";
-    LANG = "en_US.UTF-8";
+      # https://github.com/jaspervdj/hakyll/issues/614
+      # https://github.com/NixOS/nix/issues/318#issuecomment-52986702
+      # https://github.com/MaxDaten/brutal-recipes/blob/source/default.nix#L24
+      LOCALE_ARCHIVE = pkgs.lib.optionalString (pkgs.buildPlatform.libc == "glibc") "${pkgs.glibcLocales}/lib/locale/locale-archive";
+      LANG = "en_US.UTF-8";
 
-    buildPhase = ''
-      hakyll-site --verbose build
-    '';
-    installPhase = ''
-      mkdir -p "$out"
-      cp -r ../dist/* "$out"
-    '';
-  }
+      buildPhase = ''
+        hakyll-site --verbose build
+      '';
+      installPhase = ''
+        mkdir -p "$out"
+        cp -r ../dist/* "$out"
+      '';
+    }
--- nix/default.nix
+++ nix/default.nix
@@ -1,38 +1,40 @@
 let
   sources = import ./sources.nix;
 in
-  { pkgs ? import sources.nixpkgs { } }:
+{ pkgs ? import sources.nixpkgs {} }:
 
   let
     pre-commit-hooks = import sources."pre-commit-hooks.nix";
-    gitignore = import sources."gitignore.nix" { inherit (pkgs) lib; };
-    haskellPackages = pkgs.callPackage ../generator/hpkgs.nix { };
-    generator = haskellPackages.callPackage ../generator/default.nix { };
-  in {
-    generator = generator;
+    #gitignore = import sources."gitignore.nix" { inherit (pkgs) lib; };
+    #gitignore.gitignoreSource
+    haskellPackages = pkgs.callPackage ../generator/hpkgs.nix {};
+    generator = haskellPackages.callPackage ../generator/default.nix {};
+  in
+    {
+      inherit generator;
 
-    tools = [
-      generator
-      haskellPackages.ghcid
-      pkgs.cacert
-      pkgs.niv
-      pkgs.nix
-      pkgs.pre-commit
-      pre-commit-hooks.hlint
-      pre-commit-hooks.nixpkgs-fmt
-      pre-commit-hooks.ormolu
-    ];
+      tools = [
+        generator
+        haskellPackages.ghcid
+        pkgs.cacert
+        pkgs.niv
+        pkgs.nix
+        pkgs.pre-commit
+        pre-commit-hooks.hlint
+        pre-commit-hooks.nixpkgs-fmt
+        pre-commit-hooks.ormolu
+      ];
 
-    ci = {
-      pre-commit-check = pre-commit-hooks.run {
-        src = gitignore.gitignoreSource ../src;
-        hooks = {
-          nix-linter.enable = true;
-          nixpkgs-fmt.enable = true;
-          ormolu.enable = true;
-          shellcheck.enable = true;
+      ci = {
+        pre-commit-check = pre-commit-hooks.run {
+          src = ../src;
+          hooks = {
+            nix-linter.enable = true;
+            nixpkgs-fmt.enable = true;
+            ormolu.enable = true;
+            shellcheck.enable = true;
+          };
+          excludes = [ "^nix/sources\.nix$" ];
         };
-        excludes = [ "^nix/sources\.nix$" ];
       };
-    };
-  }
+    }
